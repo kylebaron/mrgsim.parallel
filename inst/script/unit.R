6ce9e8a75f1e43ec28392aa6f8a9f5b62795139c
Kyle Baron <kyleb@metrumrg.com>
2022-01-29 17:37:01 -0600

    drop timings

--- inst/script/unit.R
+++ inst/script/unit.R
@@ -7,6 +7,8 @@ library(dplyr)
 x <- test_dir("tests/testthat/")
 x <- tibble::as_tibble(x)
 x$result <- NULL
-
+x$user <- NULL
+x$system <- NULL
+x$real <- NULL
 write_csv(x = x, file = "inst/docs/tests.csv")
 
